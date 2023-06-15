8e1bf5ff6b85255cdff0f8d80243b59b2c71c155
jtpuerta <jeanthomas.puerta@free.fr>
2023-06-15 21:22:50 +0200

    test from eclipse

---
+++ src/main/java/org/jtp/test/maven/eclipse/App.java
@@ -0,0 +1,24 @@
+package org.jtp.test.maven.eclipse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOGGER = LogManager.getLogger(App.class);
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        LOGGER.trace("We've just greeted the user!");
+        LOGGER.debug("We've just greeted the user!");
+        LOGGER.info("We've just greeted the user!");
+        LOGGER.warn("We've just greeted the user!");
+        LOGGER.error("We've just greeted the user!");
+        LOGGER.fatal("We've just greeted the user!");
+    }
+}
